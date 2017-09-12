bc4bec90e561574ed0f14316481f827eddb6c491
Syoudai Yokoyama <quantumcars@gmail.com>
2017-09-13 00:08:22 +0900

    Setup project

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,47 @@ module.exports = {
     width: 1024,
     height: 768
   },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '{{ name }}',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{ description }}' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
+
   build: {
-    extend (config, { isClient }) {
+    vendor: ['vuetify'],
+    extractCSS: true,
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, { dev, isClient }) {
       // Extend only webpack config for client-bundle
       if (isClient) {
         config.target = 'electron-renderer'
+      }
+
+      if (dev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
       }
     }
   }
